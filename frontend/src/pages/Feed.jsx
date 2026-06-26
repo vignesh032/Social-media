96977bea88bfeb83a37ac0d6b1ff1252761ee1ad
vignesh032 <kyasaramvignesh03@gmail.com>
2026-06-26 21:58:13 +0530

    stories page

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { dummyPostsData } from "../assets/assets";
+import Loading from "../components/Loading";
+import StoriesBar from "../components/StoriesBar";
 
 const Feed = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default Feed
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xs:gap-8">
+      
+      {/* stories and posts */}
+      <div>
+        <StoriesBar/>
+        <div className="p-4 space-y-6">
+          List of posts
+        </div>
+      </div>
+
+      {/* right side recent msgs */}
+      <div>
+        <div>
+          <h1>Sponsered</h1>
+        </div>
+        <h1>Recent Messages</h1>
+      </div>
+
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Feed;
